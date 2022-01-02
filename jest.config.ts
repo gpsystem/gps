53b774816fe08682cbe90e4042469012e3d81cf6
nicolas377 <programnicolas@gmail.com>
2022-01-02 12:59:41 -0500

    starting off

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  preset: "ts-jest/presets/default-esm",
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  resetMocks: true,
+  setupFilesAfterEnv: ["./__tests__/jest.setup.js"],
+  modulePathIgnorePatterns: ["./__tests__/jest.setup.js"],
+  // TODO: remove this when adding a test suite
+  passWithNoTests: true,
+};
+
+export default config;
