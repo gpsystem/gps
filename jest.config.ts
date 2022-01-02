b08d517f39e7bc51af63892d6c67d7799695fcc8
nicolas377 <programnicolas@gmail.com>
2022-01-02 13:30:33 -0500

    got rid of an annoying jest warning

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,9 @@ const config: Config.InitialOptions = {
   globals: {
     "ts-jest": {
       useESM: true,
+      diagnostics: {
+        ignoreCodes: [ 'TS151001' ],
+      },
     },
   },
   moduleNameMapper: {
