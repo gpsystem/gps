5a89eecc6c6286c6a280fd5786bc2e1936a6e0f0
nicolas377 <programnicolas@gmail.com>
2022-01-08 15:05:06 -0500

    saving

---
+++ scripts/build/separatedFiles/cssFileGetter/index.ts
@@ -0,0 +1,14 @@
+import { normalizePaths } from "../../normalizePaths";
+import { matchRegexAgainstAllSourceFiles } from "../matchRegexAgainstAllSourceFiles";
+import checkAllHtmlForCssLinks from "./checkAllHtmlForCssLinks";
+
+export default function cssFileGetter(): string[] {
+  const possibleMatches = normalizePaths(
+    matchRegexAgainstAllSourceFiles(/\.css$/g)
+  );
+  const allMatches = checkAllHtmlForCssLinks(
+    normalizePaths(matchRegexAgainstAllSourceFiles(/\.html$/g))
+  );
+
+  return possibleMatches.filter((value) => allMatches.includes(value));
+}
--- scripts/build/separatedFiles/separator.ts
+++ scripts/build/separatedFiles/separator.ts
@@ -1,17 +1,20 @@
+import cssFileGetter from "./cssFileGetter";
+import tsFileGetter from "./tsFileGetter";
 import { matchRegexAgainstAllSourceFiles } from "./matchRegexAgainstAllSourceFiles";
 import type { SeparatedStructure } from "./SeparatedStructure";
-import tsFileGetter from "./tsFileGetter";
 
 /**
  * Gets the lists of the files in an object
  * @returns A list of the source files, organized by their needed actions.
  */
 export default function separator(): SeparatedStructure {
+  const toCompile = tsFileGetter();
+  const cssFiles = cssFileGetter();
   return {
-    toCompile: tsFileGetter(),
+    toCompile,
     toCopy: matchRegexAgainstAllSourceFiles(/\.(png|jpe?g|gif)$/g),
     htmlFiles: matchRegexAgainstAllSourceFiles(/\.html$/g),
-    cssFiles: matchRegexAgainstAllSourceFiles(/\.css$/g),
+    cssFiles,
     manifestJson: matchRegexAgainstAllSourceFiles(/manifest\.json$/g),
   };
 }
