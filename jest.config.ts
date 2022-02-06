02c6c0c3a0e08a388f3a8dc40914766f791b3046
nicolas377 <programnicolas@gmail.com>
2022-02-06 15:11:53 -0500

    chore: housekeeping

--- jest.config.ts
+++ jest.config.ts
@@ -5,9 +5,6 @@ const config: Config.InitialOptions = {
   globals: {
     "ts-jest": {
       useESM: true,
-      diagnostics: {
-        ignoreCodes: ["TS151001"],
-      },
     },
   },
   moduleNameMapper: {
