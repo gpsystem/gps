6f7a527d681fefbbe1fa15d6361fd8496637b842
nicolas377 <programnicolas@gmail.com>
2022-04-23 22:50:11 -0400

    build(separatedFiles): :bricks: fix the seperated files being empty
    minimatch (which glob uses) introduced a breaking change where everything has to be a forward slash

--- scripts/build/separatedFiles/matchRegexAgainstAllSourceFiles.ts
+++ scripts/build/separatedFiles/matchRegexAgainstAllSourceFiles.ts
@@ -1,11 +1,17 @@
-import { resolve } from "path";
+import { resolve, sep } from "path";
 import { sync } from "glob";
 import { extensionSourceDir as sourceDir } from "../dirNames";
 
 function matchRegexAgainstAllSourceFiles(textRegex: RegExp): string[];
 function matchRegexAgainstAllSourceFiles(): string[];
 function matchRegexAgainstAllSourceFiles(testRegex?: RegExp): string[] {
-  const allSourceFiles = sync(resolve(sourceDir + "/**"), { nodir: true });
+  // everything has to be a forward slash
+  const allSourceFiles = sync(
+    resolve(sourceDir + "/**")
+      .split(sep)
+      .join("/"),
+    { nodir: true }
+  );
 
   return testRegex
     ? allSourceFiles.filter((value) => testRegex.test(value))
